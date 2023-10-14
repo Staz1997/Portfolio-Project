66c7e3c602d7688e527c134046844a63aa3a4040
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-10-14 02:51:54 +0200

    Header Complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from "./components/Navbar"
+import Header from "./components/Header"
+import Particles from "./components/Particles"
+
 function App() {
+
   return (
-    <Navbar />
+    <>
+      <Particles />
+      <Navbar />
+      <Header />
+
+    </>
   );
 }
 
